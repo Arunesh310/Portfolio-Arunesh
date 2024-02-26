9d871158b46046f836bcdae6ed65ae36e2215c19
Nirbhay-sys <119494283+Nirbhay-sys@users.noreply.github.com>
2024-02-26 16:13:10 +0530

    minor changes in about

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -12,70 +12,65 @@ import SEO from "../data/seo";
 import "./styles/about.css";
 
 const About = () => {
-	useEffect(() => {
-		window.scrollTo(0, 0);
-	}, []);
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
 
-	const currentSEO = SEO.find((item) => item.page === "about");
+  const currentSEO = SEO.find((item) => item.page === "about");
 
-	return (
-		<>
-			<Helmet>
-				<title>{`About | ${INFO.main.title}`}</title>
-				<meta name="description" content={currentSEO.description} />
-				<meta
-					name="keywords"
-					content={currentSEO.keywords.join(", ")}
-				/>
-			</Helmet>
+  return (
+    <>
+      <Helmet>
+        <title>{`About | ${INFO.main.title}`}</title>
+        <meta name="description" content={currentSEO.description} />
+        <meta name="keywords" content={currentSEO.keywords.join(", ")} />
+      </Helmet>
 
-			<div className="page-content">
-				<NavBar active="about" />
-				<div className="content-wrapper">
-					<div className="about-logo-container">
-						<div className="about-logo">
-							<Logo width={46} />
-						</div>
-					</div>
+      <div className="page-content">
+        <NavBar active="about" />
+        <div className="content-wrapper">
+          <div className="about-logo-container">
+            <div className="about-logo">
+              <Logo width={46} />
+            </div>
+          </div>
 
-					<div className="about-container">
-						<div className="about-main">
-							<div className="about-right-side">
-								<div className="title about-title">
-									{INFO.about.title}
-								</div>
-								<div className="subtitle about-subtitle">
-									{INFO.about.description}
-								</div>
-							</div>
+          <div className="about-container">
+            <div className="about-main">
+              <div className="about-right-side">
+                <div className="title about-title">{INFO.about.title}</div>
+                <div className="subtitle about-subtitle">
+                  {INFO.about.description}
+                </div>
+              </div>
 
-							<div className="about-left-side">
-								<div className="about-image-container">
-									<div className="about-image-wrapper">
-										<img
-											src="about.png"
-											alt="about"
-											className="about-image"
-										/>
-									</div>
-								</div>
+              <div className="about-left-side">
+                <div className="about-image-container">
+                  <div className="about-image-wrapper">
+                    <img
+                      src="about.png"
+                      alt="about"
+                      className="about-image"
+                    />
+                  </div>
+                </div>
 
-								<div className="about-socials">
-									<Socials />
-								</div>
-							</div>
-						</div>
-						<div className="about-socials-mobile">
-							{/* Socials component is rendered here */}
-						</div>
-					</div>
-					<div className="page-footer">
-						<Footer />
-					</div>
-				</div>
-			</div>
-		</>
-	);
+                <div className="about-socials">
+                  <Socials />
+                </div>
+              </div>
+            </div>
+            <div className="about-socials-mobile">
+              <Socials />
+            </div>
+          </div>
+          <div className="page-footer">
+            <Footer />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default About;
